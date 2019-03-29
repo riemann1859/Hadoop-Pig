70935f9032d50ac241c0c5a0ce62622f2cad1383
riemann1859 <44736451+riemann1859@users.noreply.github.com>
2019-03-29 12:33:45 +0300

    Mapper for Question 1

--- 3.mapper_genre_counts.py
+++ 3.mapper_genre_counts.py
@@ -6,7 +6,9 @@
 
 import sys
 
-# want to count the number of occurrences of each  genre
+
+#key : genre
+#value: 1
 
 for line in sys.stdin:
     for genre in line.strip().split(":")[-1].split("|"):
