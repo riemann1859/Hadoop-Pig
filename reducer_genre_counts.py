5d95544dbb95474dcc54867140ecf98cf7fd70ca
riemann1859 <44736451+riemann1859@users.noreply.github.com>
2019-03-29 12:17:12 +0300

    Update reducer_genre_counts.py

--- reducer_genre_counts.py
+++ reducer_genre_counts.py
@@ -1,24 +1,22 @@
 
-# coding: utf-8
 
-# In[ ]:
-
-
-#!/usr/bin/python
-
-import sys
-
-genre_dict={}
-for line in sys.stdin:
-    data=line.strip().split("\t")
-    if len(data)!=2:
-        continue
-    else:
-        if data[0] not in genre_dict.keys():
-            genre_dict[data[0]]=1
-        else:
-            genre_dict[data[0]]+=1
-
-for genre,count list(in genre_dict.items()).sort(key=lambda x:x[1],reverse=True):
-    print "{x}\t{y}"(x=genre,y=count)
-
+#!/usr/bin/env python                                                                                                                                                   
+                                                                                                                                                                        
+import sys                                                                                                                                                              
+                                                                                                                                                                        
+genre_dict={}                                                                                                                                                           
+for line in sys.stdin:                                                                                                                                                  
+    data=line.strip().split("\t")                                                                                                                                       
+    if len(data)!=2:                                                                                                                                                    
+        continue                                                                                                                                                        
+    else:                                                                                                                                                               
+        if data[0] not in genre_dict.keys():                                                                                                                            
+            genre_dict[data[0]]=1                                                                                                                                       
+        else:                                                                                                                                                           
+            genre_dict[data[0]]+=1                                                                                                                                      
+                                                                                                                                                                        
+a=list(genre_dict.items())                                                                                                                                              
+a.sort(key=lambda x:x[1],reverse=True)                                                                                                                                  
+                                                                                                                                                                        
+for genre,count in a:                                                                                                                                                   
+    print("{x}\t{y}".format(x=genre,y=count)) 
