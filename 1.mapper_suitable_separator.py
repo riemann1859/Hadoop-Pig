02d34eeaf6cc6a61e0dce41ac6f2307a43a7e853
riemann1859 <44736451+riemann1859@users.noreply.github.com>
2019-03-29 12:28:02 +0300

    mapper for cleaning movies.dat  dataset

--- 1.mapper_suitable_separator.py
+++ 1.mapper_suitable_separator.py
@@ -6,7 +6,7 @@
 # In our movies data columns are separated by ::
 # In loading data via Pig Storage this may cause some difficulties
 # By means of this mapper we replace the delimiter :: by :
-# ın this MapReduce process we do not need any reducer
+# In this MapReduce process we do not need any reducer
 
 import sys
 
